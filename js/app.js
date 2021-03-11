4b548daf687a9e6f292a654cf37d3036e30aeeb7
Zihad Malik <zihadm654@gamil.com>
2021-03-11 16:50:10 +0600

    initial message

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+// // for(let i = 0; i < 10; i++) {
+// //     console.log(i);
+// // }
+// // function square (number) {
+// //     return number * number;
+// // }
+// // const result = square(5);
+// // console.log(result);
+
+//     const sayHi = (name) => {
+//         console.log(`Hi, ${name}!`);
+//     }
+
+//     sayHi('zihad');
+//     sayHi('jane');
+
+//     const add = (a, b) => {
+//         return a + b;
+//     }
+//     const sum = add(2, 2);
+//     console.log(sum);
+
+
+// const square = (number) => {
+//     return number + number;
+// }
+// const result = square(10);
+// console.log(result);
